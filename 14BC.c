612754ef9280d779219bf23f16f45688cafd1689
Shivesh kumar <152985427+shivesh005@users.noreply.github.com>
2023-12-26 15:49:35 +0530

    Add files via upload

---
+++ 14BC.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+int main (){
+    char a[100]="shivesh";
+    char b[100]="nikhil";
+
+ printf("%d\n",strrev(b,a));
+printf("%d\n",strcat(b,a));
+return 0;
+}
